f7134929aa4a07395334c2a3ac6b6b5ab8a1a166
lisz1012 <lisz1012@163.com>
2020-03-02 01:06:34 -0800

    Async callbacks

--- src/main/java/com/lisz/zookeeper/WatchCallback.java
+++ src/main/java/com/lisz/zookeeper/WatchCallback.java
@@ -7,36 +7,72 @@ import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
+import java.util.concurrent.CountDownLatch;
+
 public class WatchCallback implements Watcher, AsyncCallback.StatCallback, AsyncCallback.DataCallback {
     private ZooKeeper zk;
-    private MyConf myConf;
+    private MyConf conf;
+    private CountDownLatch latch = new CountDownLatch(1);
 
     public WatchCallback(ZooKeeper zk) {
         this.zk = zk;
     }
 
-    public MyConf getMyConf() {
-        return myConf;
+    public MyConf getConf() {
+        return conf;
     }
 
-    public void setMyConf(MyConf myConf) {
-        this.myConf = myConf;
+    public void setConf(MyConf conf) {
+        this.conf = conf;
+    }
+
+    public void await() {
+        zk.exists("/AppConf", this, this, "ABC");
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
-
+        if (data != null) {
+            String s = new String(data);
+            conf.setConf(s);
+            latch.countDown();
+        }
     }
 
     @Override
     public void processResult(int rc, String path, Object ctx, Stat stat) {
-        if (stat != null) { // stat != null 代表节点已经存在了
+        if (stat != null) { // stat != null 代表节点已经存在了, 那就再拿数据，再次回调
             zk.getData("/AppConf", this, this, "sdfs");
         }
     }
 
     @Override
     public void process(WatchedEvent event) {
-
+        switch (event.getType()) {
+            case None:
+                break;
+            case NodeCreated:
+                zk.getData("/AppConf", this, this, "sdfs");
+                break;
+            case NodeDeleted:
+                // 容忍性
+                conf.setConf("");
+                latch = new CountDownLatch(1);
+                break;
+            case NodeDataChanged:
+                zk.getData("/AppConf", this, this, "sdfs");
+                break;
+            case NodeChildrenChanged:
+                break;
+            case DataWatchRemoved:
+                break;
+            case ChildWatchRemoved:
+                break;
+        }
     }
 }
--- src/main/java/com/lisz/zookeeper/config/TestConfig.java
+++ src/main/java/com/lisz/zookeeper/config/TestConfig.java
@@ -1,13 +1,12 @@
 package com.lisz.zookeeper.config;
 
 import com.lisz.zookeeper.WatchCallback;
-import org.apache.zookeeper.AsyncCallback;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestConfig {
     private ZooKeeper zk;
@@ -27,10 +26,31 @@ public class TestConfig {
         }
     }
 
+    @Test
     public void getConf() {
         myConf = new MyConf();
         WatchCallback watchCallback = new WatchCallback(zk);
-        watchCallback.setMyConf(myConf);
-        zk.exists("/AppConf", watchCallback, watchCallback, "ABC");
+        watchCallback.setConf(myConf);
+
+        // 无论节点存在不存在，都去取数据，取到了再往下走
+        watchCallback.await();
+        // 1。 节点不存在
+        // 2。 节点存在
+        // 并没有把每种场景都线性写代码把堆积起来，而是通过callback和watch把各个部分代码粘连起来
+
+        while (true) {
+            if (myConf.getConf().equals("")) {
+                System.out.println("conf 丢了 。。。");
+                watchCallback.await();
+            } else {
+                System.out.println(myConf.getConf());
+            }
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
