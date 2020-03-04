8c8880f01ee4b8533367438f4d1cfd12c2a43ea3
lisz1012 <lisz1012@163.com>
2020-03-03 22:12:56 -0800

    Distributed Lock

--- src/main/java/com/lisz/zookeeper/lock/WatchCallback.java
+++ src/main/java/com/lisz/zookeeper/lock/WatchCallback.java
@@ -54,6 +54,7 @@ public class WatchCallback implements Watcher, AsyncCallback.StringCallback, Asy
         latch = new CountDownLatch(1);
     }
 
+    // Watcher
     @Override
     public void process(WatchedEvent event) {
         switch (event.getType()) {
@@ -75,6 +76,7 @@ public class WatchCallback implements Watcher, AsyncCallback.StringCallback, Asy
         }
     }
 
+    // StringCallback
     @Override
     public void processResult(int rc, String path, Object ctx, String name) {
         //System.out.println(path);
@@ -87,9 +89,10 @@ public class WatchCallback implements Watcher, AsyncCallback.StringCallback, Asy
 
     }
 
+    // Children2Callback
     @Override
     public void processResult(int rc, String path, Object ctx, List<String> children, Stat stat) {
-        //进到这里说明创建节点成功，看到的锁目录下的所有节点，且看到了自己和前面的所有节点
+        //进到这里说明StringCallback.processResult创建节点成功，看到的锁目录下的所有节点，且看到了自己和前面的所有节点
         System.out.println(threadName + " looks locks ... ");
         /*for (String child : children) {
             System.out.println(child);
@@ -99,13 +102,13 @@ public class WatchCallback implements Watcher, AsyncCallback.StringCallback, Asy
         if (index == 0) {
             System.out.println(threadName + " I'm the first...");
             try {
-                zk.setData("/", threadName.getBytes(), -1); // 加了这么一步：谁获得锁了，就把线程信息写到锁目录里，让第一个线程别跑太快。但更关键的是重入锁，抢到了就做个标记
+                zk.setData("/", threadName.getBytes(), -1); // 加了这么一步：谁获得锁了，就把线程信息写到锁目录里，让第一个线程别跑太快。但更关键的是重入锁，抢到了就做个标记。设置数这里不需要callback
             } catch (KeeperException e) {
                 e.printStackTrace();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            latch.countDown();
+            latch.countDown(); //是当前队列的第一个，让 tryLock()继续往下走，并顺利退出，开始干活的业务
         } else {
             // 盯住index - 1，也就是前一个
             // 这里的这个watcher一定要写，不能是false，第二个this也一定要写，判断exits的一瞬间前面的节点挂（超时消失）了，有可能exists监控成功有可能不成功
@@ -113,8 +116,9 @@ public class WatchCallback implements Watcher, AsyncCallback.StringCallback, Asy
         }
     }
 
+    // StatCallback
     @Override
     public void processResult(int rc, String path, Object ctx, Stat stat) {
-
+        // stat != null 代表zk.exists方法发现节点已经存在了
     }
 }
